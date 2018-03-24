1d9ed5cb07239f83a05db0425ff2e83626c5835e
Chernysh Vladimir <chernysh.vladimir@apriorit.com>
2018-03-24 15:14:14 +0200

    wrote complex.lib

--- ComplexDemo/ComplexLib/ComplexLib.cpp
+++ ComplexDemo/ComplexLib/ComplexLib.cpp
@@ -4,3 +4,66 @@
 
 #include "ComplexLib.h"
 
+namespace ComplexLib
+{
+	Complex::Complex() {}
+	Complex::Complex(double r) :m_real(r), m_imaginary(0) {}
+	Complex::Complex(double r, double i) : m_real(r), m_imaginary(i) {}
+	Complex::Complex(const Complex & val): m_real(val.m_real), m_imaginary(val.m_imaginary) {}
+	Complex::~Complex() {}
+
+	Complex& ComplexLib::Complex::operator =(Complex &lval)
+	{
+		if (lval.m_real != this->m_real)
+		{
+			m_real = lval.m_real;
+		}
+		if (lval.m_imaginary != this->m_imaginary)
+		{
+			m_imaginary = lval.m_imaginary;
+		}
+		
+		return (*this);
+	}
+	Complex& Complex::operator += (Complex &lval)
+	{
+		m_real += lval.m_real;
+		m_imaginary += lval.m_imaginary;
+		return *this;
+	}
+	Complex Complex::operator + (const Complex &lval)
+	{
+		return Complex(m_real + lval.m_real, m_imaginary + lval.m_imaginary);
+	}
+	Complex Complex::operator - (const Complex &lval)
+	{
+		return Complex(m_real - lval.m_real, m_imaginary - lval.m_imaginary);
+	}
+	Complex Complex::operator * (const Complex &lval)
+	{
+		return Complex(m_real * lval.m_real - m_imaginary * lval.m_imaginary, m_real * lval.m_imaginary + m_imaginary * lval.m_real);
+	}
+	Complex Complex::operator/(const Complex &lval)
+	{
+		Complex temp;
+
+		double r = lval.m_real * lval.m_real + lval.m_imaginary * lval.m_imaginary;
+		temp.m_real = (m_real * lval.m_real + m_imaginary * lval.m_imaginary) / r;
+		temp.m_imaginary = (m_imaginary * lval.m_real - m_real * lval.m_imaginary) / r;
+
+		return temp;
+	}
+
+	Complex::operator double()
+	{
+		return this->get_module();
+	}
+
+	double Complex::get_module()
+	{
+		return sqrt(m_real * m_real + m_imaginary * m_imaginary);
+	}
+
+
+}
+
--- ComplexDemo/ComplexLib/ComplexLib.h
+++ ComplexDemo/ComplexLib/ComplexLib.h
@@ -1,13 +1,27 @@
 // ComplexLib.h
-
+#include <iostream>
+#include <cmath>
 #pragma once
-
-using namespace System;
 
 namespace ComplexLib {
 
-	public ref class Class1
+	class Complex
 	{
-		// TODO: Add your methods for this class here.
+	public:
+		Complex();
+		Complex(double real);
+		Complex(double real, double imaginary);
+		Complex(const Complex & lval);
+		~Complex();
+		Complex& operator = (Complex &lval);
+		Complex& operator += (Complex &lval);
+		Complex operator + (const Complex &lval);
+		Complex operator - (const Complex &lval);
+		Complex operator * (const Complex &lval);
+		Complex operator / (const Complex &lval);
+		operator double();
+		double get_module();
+	private:
+		double m_real, m_imaginary;
 	};
 }
